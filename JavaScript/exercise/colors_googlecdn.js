f5146e829944d67d2412ea7135f0e3b7b40a003d
yongchoooon <sod7050@gmail.com>
2022-07-12 23:40:12 +0900

    TIL : JS_Basic

---
+++ JavaScript/exercise/colors_googlecdn.js
@@ -0,0 +1,38 @@
+var Links = {
+  SetColor : function (color) {
+    // var alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i < alist.length) {
+    //   alist[i].style.color = color;
+    //   i = i + 1;
+    //   }
+    $('a').css('color', color);
+  }
+}
+
+var Body = {
+  SetColor : function (color) {
+    //document.querySelector('body').style.color = color;
+    $('body').css('color', color);
+  },
+  SetBackgroundColor : function (color) {
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
+  }
+}
+
+function NightDayHandler(self){
+  if(self.value === 'night') {
+    Body.SetBackgroundColor('black');
+    Body.SetColor('white');
+    self.value='day';
+
+    Links.SetColor('yellow');
+  } else {
+    Body.SetBackgroundColor('white');
+    Body.SetColor('black');
+    self.value='night';
+
+    Links.SetColor('red');
+  }
+}
